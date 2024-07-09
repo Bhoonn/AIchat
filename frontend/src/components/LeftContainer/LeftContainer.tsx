8a7fac9ce76b8f8382f8a6a6cb7bddcd20729a89
Bhoon <62685365+Bhoonn@users.noreply.github.com>
2024-07-09 12:52:36 +0200

    fix: make the upload fancy

--- frontend/src/components/LeftContainer/LeftContainer.tsx
+++ frontend/src/components/LeftContainer/LeftContainer.tsx
@@ -2,16 +2,25 @@ import { useState } from "react";
 import "./LeftContainer.css"
 
 const LeftContainer = () => {
+  const [draggingOver, setDraggingOver] = useState(false);
+
   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     const droppedFile = e.dataTransfer.files[0];
     if (droppedFile) {
       uploadFile(droppedFile);
     }
+    setDraggingOver(false)
   };
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
+    setDraggingOver(true)
+  };
+
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDraggingOver(false)
   };
 
   const uploadFile = (file: File) => {
@@ -40,8 +49,13 @@ const LeftContainer = () => {
   };
 
     return <div id="Left">
-      <div className="drop-zone" onDrop={handleDrop} onDragOver={handleDragOver}>
-        <p>Drag & Drop your document here</p>
+      <div className={`drop-zone ${draggingOver ? 'dragging-over' : ''}`} onDrop={handleDrop} onDragOver={handleDragOver} onDragLeave={handleDragLeave}>
+        <div className="drop-zone-container">
+          <img src="./images/upload.png"></img>
+          <p>Drag & drop files or <span className="browseBtn">Browse</span></p>
+          <p>Supported formats: .doc, .docx, .pdf</p>
+        </div>
+        <input type="file" className="hidden-fill-input" id="fileInput" accept=".doc,.docx,.pdf"></input>
       </div>
     </div>
 }
